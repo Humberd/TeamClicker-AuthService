16b841ab995a9d5986ce9afef256ced2f76d8223
Maciej Sawicki <sawik11@wp.pl>
2018-04-08 20:16:31 +0200

    Added jwt data to swagger ignored types

--- src/main/kotlin/com/teamclicker/authservice/beans/SwaggerBean.kt
+++ src/main/kotlin/com/teamclicker/authservice/beans/SwaggerBean.kt
@@ -1,6 +1,7 @@
 package com.teamclicker.authservice.beans
 
 import com.google.common.base.Predicates
+import com.teamclicker.authservice.security.JWTData
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
@@ -10,7 +11,6 @@ import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +22,7 @@ class SwaggerBean {
             .apiInfo(DEFAULT_API_INFO)
             .produces(DEFAULT_PRODUCES)
             .consumes(DEFAULT_CONSUMES)
-//                .ignoredParameterTypes(UserDAO::class.java)
+            .ignoredParameterTypes(JWTData::class.java)
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
