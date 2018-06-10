59beb014643be53de830b8f35bac5c34216bc681
Maciej Sawicki <sawik11@wp.pl>
2018-06-10 02:54:11 +0200

    Removed unneccessary dependencies

--- src/main/kotlin/com/teamclicker/authservice/AuthServiceApplication.kt
+++ src/main/kotlin/com/teamclicker/authservice/AuthServiceApplication.kt
@@ -2,7 +2,6 @@ package com.teamclicker.authservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.stream.annotation.EnableBinding
 
 @SpringBootApplication
 class AuthserviceApplication
