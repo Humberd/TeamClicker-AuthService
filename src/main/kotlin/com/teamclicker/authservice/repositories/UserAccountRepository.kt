61627034ed4d2b6fface7071b88f4cb28a21c6b2
Humberd <humberd2@gmail.com>
2018-07-08 13:02:43 +0200

    Added JPA Persistance

--- src/main/kotlin/com/teamclicker/authservice/repositories/UserAccountRepository.kt
+++ src/main/kotlin/com/teamclicker/authservice/repositories/UserAccountRepository.kt
@@ -1,6 +1,7 @@
 package com.teamclicker.authservice.repositories
 
 import com.teamclicker.authservice.dao.UserAccountDAO
+import org.intellij.lang.annotations.Language
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
